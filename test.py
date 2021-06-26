950fe9c557f0ea1b4b9e3f3d7a2c4608aeacd3a6
alexnakov <59810189+alexnakov@users.noreply.github.com>
2021-06-26 10:58:51 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -4,6 +4,25 @@ from pygame.locals import *
 from constants import *
 
 
+class CollisionRectangle:
+    def __init__(self, orientation, length, width, x, y):
+        """
+        :param orientation: "H" for horizontal, "V" for vertical
+        :param length: match the length of the side of the rectangle
+        :param width: 2px to 4px sensible width for collision detection between shapes
+        :param x: the x coord of top-left or top-right corner of the paddle or brick (orientation dependent)
+        :param y: the y coord of top-left or top-right corner of the paddle or brick (orientation dependent)
+        """
+        self.orientation = orientation
+        self.length = length
+        self.width = width
+        self.x, self.y = x, y
+
+        if self.orientation == "H":
+            self.top_left = (self.x, self.y)
+
+
+
 class Ball:
     def __init__(self, x, y, radius, initial_velocity, time):
         self.x, self.y = x, y
@@ -25,10 +44,10 @@ class Ball:
 
     def check_for_collision_with_paddle(self, paddle_object):
         shortest_possible_distance_between_ball_paddle = (paddle_object.height / 2) + self.radius
-        if (paddle_object.centre_y - self.y) <= shortest_possible_distance_between_ball_paddle:
+        if (paddle_object.centre_y - self.y) <= shortest_possible_distance_between_ball_paddle and \
+                paddle_object.x <= self.x <= paddle_object.x + paddle_object.width:
             self.colliding = True
             self.velocity[1] = -self.velocity[1]
-
 
 
 class Paddle:
@@ -46,7 +65,7 @@ root = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 
 paddle = Paddle(350, 650, 200, 50)
-ball = Ball(400, 400, 10, (0, 10), 5)
+ball = Ball(200, 400, 10, (5, 10), 5)
 
 while True:
     for event in pygame.event.get():
