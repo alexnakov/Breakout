1b79f7f13f9472c141357f14534752b0192fbef5
alexnakov <59810189+alexnakov@users.noreply.github.com>
2021-06-26 10:28:22 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -20,14 +20,23 @@ class Ball:
         dx, dy = normalised_vector[0], normalised_vector[1]
         self.x += dx * self.time
         self.y += dy * self.time
-        x_new, y_new = int(self.x), int(self.y)
+        x_new, y_new = round(self.x), round(self.y)
         pygame.draw.circle(root, RED, (x_new, y_new), self.radius)
+
+    def check_for_collision_with_paddle(self, paddle_object):
+        shortest_possible_distance_between_ball_paddle = (paddle_object.height / 2) + self.radius
+        if (paddle_object.centre_y - self.y) <= shortest_possible_distance_between_ball_paddle:
+            self.colliding = True
+            self.velocity[1] = -self.velocity[1]
+
 
 
 class Paddle:
     def __init__(self, x, y, width, height):
         self.x, self.y = x, y
         self.width, self.height = width, height
+        self.centre_x = self.x + (self.width / 2)
+        self.centre_y = self.y + (self.height / 2)
 
     def update(self):
         pygame.draw.rect(root, NAVY, Rect(self.x, self.y, self.width, self.height))
@@ -45,6 +54,7 @@ while True:
             pygame.quit()
             sys.exit()
     root.fill(BLACK)
+    ball.check_for_collision_with_paddle(paddle)
     paddle.update()
     ball.update()
     pygame.display.update()
