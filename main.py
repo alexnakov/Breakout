29efc2a5e63d06d15a40c3d9d15b95b95194b45f
alexnakov <59810189+alexnakov@users.noreply.github.com>
2021-06-27 20:21:39 +0100

    Ball projected from paddle goes in random directions
    I can say the basic of the game are pretty much completed

--- main.py
+++ main.py
@@ -2,40 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 from constants import *
-
-
-class PaddleDownToUpLine:
-    """ This is a horizontal line which will act as the paddle to test collisions. """
-
-    def __init__(self, length, x, y):
-        """
-        The horizontal line will be 1 px long for now
-        :param length: Length of line in pixels
-        :param x: The x coord of the left end
-        :param y: The y coord of the left end
-        """
-        self.length = length
-        self.left_x, self.left_y = x, y
-        self.colliding = False
-
-    def draw(self):
-        pygame.draw.line(root, BLUE, (self.left_x, self.left_y), (self.left_x + self.length, self.left_y))
-
-    def check_collision(self, ball_object):
-        if not self.colliding and self.left_x <= ball_object.centre_x <= self.left_x + self.length \
-                and ball_object.centre_y + ball_object.radius >= self.left_y:
-            ball_object.velocity[1] = -ball_object.velocity[1]
-            self.colliding = True
-        elif self.colliding and not (self.left_x <= ball_object.centre_x <= self.left_x + self.length) \
-                or not (ball_object.centre_y + ball_object.radius >= self.left_y):
-            self.colliding = False
-
-    def update(self):
-        mouse_x = pygame.mouse.get_pos()[0]
-        self.left_x = mouse_x - self.length / 2
-
-        self.check_collision(ball)
-        self.draw()
+import random
 
 
 class RightToLeftVerticalCollisionLine:
@@ -112,7 +79,7 @@ class LeftToRightVerticalCollisionLine:
 class DownToUpHorizontalCollisionLine:
     """ This is a horizontal line which will act as the paddle to test collisions. """
 
-    def __init__(self, length, x, y, surface):
+    def __init__(self, length, x, y, surface, change_colliding_object_velocity=None):
         """
         The horizontal line will be 1 px long for now
         :param length: Length of line in pixels
@@ -123,6 +90,7 @@ class DownToUpHorizontalCollisionLine:
         self.length = length
         self.left_x, self.left_y = x, y
         self.surface = surface
+        self.change_colliding_object_velocity = change_colliding_object_velocity
         self.colliding = False
         self.number_collisions = 0
 
@@ -132,7 +100,10 @@ class DownToUpHorizontalCollisionLine:
     def check_collision(self, ball_object):
         if not self.colliding and self.left_x <= ball_object.centre_x <= self.left_x + self.length \
                 and ball_object.centre_y + ball_object.radius >= self.left_y >= ball_object.centre_y:
-            ball_object.velocity[1] = -ball_object.velocity[1]
+            if not self.change_colliding_object_velocity:
+                ball_object.velocity[1] = -ball_object.velocity[1]
+            else:
+                ball_object.velocity[0], ball_object.velocity[1] = random.uniform(-1.2, 1.2), -1
             self.colliding = True
             self.number_collisions += 1
         elif self.colliding and not (self.left_x <= ball_object.centre_x <= self.left_x + self.length) \
@@ -244,7 +215,7 @@ class Paddle:
 
         self.right_wall = LeftToRightVerticalCollisionLine(self.height, self.x + self.length, self.y, screen)
         self.left_wall = RightToLeftVerticalCollisionLine(self.height, self.x, self.y, screen)
-        self.top_wall = DownToUpHorizontalCollisionLine(self.length, self.x, self.y, screen)
+        self.top_wall = DownToUpHorizontalCollisionLine(self.length, self.x, self.y, screen, True)
         self.bottom_wall = UpToDownHorizontalCollisionLine(self.length, self.x, self.y + self.height, screen)
 
     def draw(self):
@@ -284,16 +255,10 @@ bricks = []
 
 for j in range(1, 9):
     for i in range(1, 16):
-        # screen.blit(box, (-60 + 66*i, -6 + 36*j))
         bricks.append(Brick(-60 + 66*i, -6 + 36*j, BRICK_LENGTH, BRICK_HEIGHT, screen))
 
-# brick1 = Brick(200, 200, 300, 300, screen)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+def update_screen():
     screen.fill(BLACK)
     screen_right_wall.update()
     screen_left_wall.update()
@@ -304,8 +269,15 @@ while True:
         if any([brick.bottom_wall.number_collisions, brick.top_wall.number_collisions,
                 brick.left_wall.number_collisions, brick.right_wall.number_collisions]):
             bricks.remove(bricks[bricks.index(brick)])
-    # brick1.update()
     ball.update()
     root.blit(screen, (12, 12))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    update_screen()
     pygame.display.update()
     clock.tick(50)
