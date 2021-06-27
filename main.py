315e291e94f0e7defbbd987aa4dddab4bbd7b178
alexnakov <59810189+alexnakov@users.noreply.github.com>
2021-06-27 09:47:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -178,19 +178,14 @@ class Ball:
         self.time = time
         self.velocity = pygame.Vector2(initial_velocity)
 
-    def draw(self):
-        pygame.draw.circle(root, RED, (self.centre_x, self.centre_y), self.radius)
-
     def update(self):
         dx, dy = pygame.Vector2.normalize(self.velocity)
-        # distance = speed * time
         self.centre_x += dx * self.time
         self.centre_y += dy * self.time
-        x_new, y_new = round(self.centre_x), round(self.centre_y)
-        pygame.draw.circle(root, RED, (x_new, y_new), self.radius)
+        pygame.draw.circle(screen, RED, (round(self.centre_x), round(self.centre_y)), self.radius)
 
 
-class Block:
+class Brick:
     """ This will be a rectangle where collisions with a ball is possible """
 
     def __init__(self, x, y, length, height):
@@ -220,16 +215,17 @@ class Block:
 
 
 root = pygame.display.set_mode((1020, 624))
+clock = pygame.time.Clock()
+
 root.fill(GRAY)
 
 screen = pygame.Surface((996, 600))
 
-ball = Ball(400, 900, 5, (1, 1), 0.001)
+ball = Ball(50, 50, 1, (1, 1), 1)
 
 # for j in range(1, 9):
 #     for i in range(1, 16):
 #         screen.blit(box, (-60 + 66*i, -6 + 36*j))
-
 
 root.blit(screen, (12, 12))
 
@@ -238,4 +234,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    screen.fill(BLACK)
+    ball.update()
+    root.blit(screen, (12, 12))
     pygame.display.update()
+    clock.tick(60)
